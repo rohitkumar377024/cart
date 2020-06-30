84fc66cb64fbdbf5dc97aa3e250541ec5778a03e
Rohit <rohitkumar377024@gmail.com>
2020-06-30 12:40:49 +0530

    Added: List of Products in useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([
+    {
+      name: 'Samsung Galaxy Buds+',
+      price: 15000,
+      id: 1
+    },
+    {
+      name: 'Oneplus 8 Pro',
+      price: 60000,
+      id: 2
+    },
+    {
+      name: 'Boat Bassheads 100',
+      price: 400,
+      id: 3
+    }
+  ]);
+
   return (
     <div className="App">
       <h1>Cart.</h1>
+
+      <h3>Products</h3>
+      {products.map(product => (
+        <li>{product.name}</li>
+      ))}
     </div>
   );
 }
